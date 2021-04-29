dbd790c7831e56cb60019b962faaf81ac720f02d
SantiFagliano <fagliano.santi@gmail.com>
2021-04-29 15:16:19 -0300

    add new page of certifications and licenses

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,10 @@
-/*!
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
+(function ($) {
+    "use strict";
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
+            this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
             var target = $(this.hash);
@@ -29,17 +23,13 @@
             }
         }
     });
-
-    // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
     });
-
-    // Activate scrollspy to add active class to navbar items on scroll
     $("body").scrollspy({
         target: "#sideNav",
     });
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     });
-})(jQuery); // End of use strict
+})(jQuery);
